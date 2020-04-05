0b2dffc521dc7924f1f1509f8afb37a33685e17b
Ammar Naqvi <ammarabbasnaqvi@outlook.com>
2020-04-05 23:29:43 +0500

    style category a bit

--- src/components/category.js
+++ src/components/category.js
@@ -1,9 +1,12 @@
 import React from "react"
 
 const Category = ({ category }) => (
-    <article className="w-64 p-5 mt-8 bg-white">
-        <h3>{category.title}</h3>
+    <article className="relative w-64 p-5 mt-8 bg-white">
+        <h3 className="mb-8 text-lg font-semibold text-gray-700">
+            {category.title}
+        </h3>
         <a
+            className="absolute bottom-0 right-0 m-2 text-sm text-gray-800"
             target="_blank"
             rel="noopener noreferrer"
             href={`http://www.netflix.com/browse/genre/${category.id}`}
