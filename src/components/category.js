5f2295815124a59f4b79148f9ddeef0cc2b42730
Ammar Naqvi <ammarabbasnaqvi@outlook.com>
2020-04-05 23:24:00 +0500

    open netflix in new tab

--- src/components/category.js
+++ src/components/category.js
@@ -3,7 +3,11 @@ import React from "react"
 const Category = ({ category }) => (
     <article className="w-64 p-5 mt-8 bg-white">
         <h3>{category.title}</h3>
-        <a href={`http://www.netflix.com/browse/genre/${category.id}`}>
+        <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`http://www.netflix.com/browse/genre/${category.id}`}
+        >
             View on Netflix &rarr;
         </a>
     </article>
